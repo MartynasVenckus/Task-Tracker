8d65c120b6250745efd0f0fb97e3026b7b310e57
Martynas Venckus <martven777@gmail.com>
2021-06-27 23:37:43 +0300

    Add routes

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,8 @@
 <template>
     <header>
         <h1>{{ title }}</h1>
-        <Button @btn-click="$emit('toggle-add-task')" 
+        <Button v-show="homePage"
+            @btn-click="$emit('toggle-add-task')" 
             :text="showAddTask ? 'Close' : 'Add task'"
             :color="showAddTask ? 'red' : 'green'"
         />
@@ -18,7 +19,16 @@ export default {
         showAddTask: Boolean,
     },
     components: {
-        Button
+        Button,
+    },
+    computed: {
+        homePage() {
+            if(this.$route.path === '/') {
+                return true
+            } else {
+                return false
+            }
+        }
     }
 }
 </script>
