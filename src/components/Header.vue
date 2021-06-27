c238090a3222b0b6291542a7d2cbebcf8e9425c2
Martynas Venckus <martven777@gmail.com>
2021-06-27 19:33:21 +0300

    Add task addition imitation

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <header>
         <h1>{{ title }}</h1>
-        <Button text="Add task" color="black" />
+        <Button text="Add task" color="green" />
     </header>
 </template>
 
